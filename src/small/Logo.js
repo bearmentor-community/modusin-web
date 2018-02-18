0988b223a7908c5d596b0aa4b3cbda355b056a73
M Haidar Hanif <git@mhaidarhanif.com>
2018-02-18 22:03:29 +0700

    Normalize logo style

--- src/small/Logo.js
+++ src/small/Logo.js
@@ -1,16 +1,9 @@
 import React from "react"
 import { Link } from "react-router-dom"
 
-const style = {
-  textAlign: "center",
-  fontWeight: "bold",
-  textDecoration: "none",
-  color: "black"
-}
-
 const Logo = props => (
-  <Link to="/" style={style}>
-    <h1>Modusin</h1>
+  <Link to="/">
+    <h1 className="logo-text bold">Modusin</h1>
   </Link>
 )
 
