ea6ef039a9f1ad9f7876ba57511425a2d3ff66ab
M Haidar Hanif <git@mhaidarhanif.com>
2018-02-19 05:00:25 +0700

    Refine SegmentProfile positioning

--- src/large/SegmentProfile.js
+++ src/large/SegmentProfile.js
@@ -11,41 +11,36 @@ import ActionButton from "../small/ActionButton"
 
 const SegmentProfile = ({ profile }) => {
   return (
-    <Row id="SegmentProfile" className="center">
-      <Col xs={12} md={8}>
-        <Row>
-          <Col>
-            <Link to={`/profile/${profile.username}`}>
-              <PageTitle>@{profile.username}</PageTitle>
-            </Link>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <PageSubtitle>"name"</PageSubtitle>
-            <PageSubtitle>"bio"</PageSubtitle>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Link to="/settings">
-              <Button>Edit My Profile</Button>
-            </Link>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <div className="m-3">
-              <h3>"list of posts"</h3>
-              <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-              </ul>
-            </div>
-          </Col>
-        </Row>
-      </Col>
+    <Row id="SegmentProfile" className="center " xs={12} md={8}>
+      <div>
+        <Col>
+          <Link to={`/profile/${profile.username}`}>
+            <PageTitle>@{profile.username}</PageTitle>
+          </Link>
+        </Col>
+
+        <Col>
+          <PageSubtitle>"name"</PageSubtitle>
+          <PageSubtitle>"bio"</PageSubtitle>
+        </Col>
+
+        <Col>
+          <Link to="/settings">
+            <Button>Edit My Profile</Button>
+          </Link>
+        </Col>
+
+        <Col>
+          <div className="m-3">
+            <h3>"list of posts"</h3>
+            <ul>
+              <li>1</li>
+              <li>2</li>
+              <li>3</li>
+            </ul>
+          </div>
+        </Col>
+      </div>
     </Row>
   )
 }
