9510e51a8e46bacf3a0776e2fcd64293cd72cf74
M Haidar Hanif <git@mhaidarhanif.com>
2018-02-19 01:15:17 +0700

    Rename all pages to title case

--- src/medium/ActionButtons.js
+++ src/medium/ActionButtons.js
@@ -4,7 +4,7 @@ import ActionButton from "../small/ActionButton"
 
 const ActionButtons = props => (
   <span className="action buttons">
-    <ActionButton to="/create">Create Post</ActionButton>
+    <ActionButton to="/new">Write New Post</ActionButton>
     <ActionButton to="/register">Register</ActionButton>
     <ActionButton to="/login">Login</ActionButton>
   </span>
--- src/templates/Layout.js
+++ src/templates/Layout.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Container, Row, Col } from "reactstrap"
+import { Container } from "reactstrap"
 
 import SegmentDebug from "../large/SegmentDebug"
 import SegmentMain from "../large/SegmentMain"
