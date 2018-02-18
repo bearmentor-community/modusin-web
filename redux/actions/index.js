bcd68532a2c9222ca7107b89a7e3df6eff40ca97
M Haidar Hanif <git@mhaidarhanif.com>
2018-02-19 05:24:08 +0700

    Define action creators & types

---
+++ redux/actions/index.js
@@ -0,0 +1,86 @@
+import {
+  GET_FEATURED_POSTS,
+  GET_POSTS,
+  GET_TOPICS,
+  SELECT_POST,
+  SELECT_TOPIC,
+  SELECT_PROFILE,
+  ACCOUNT_REGISTER,
+  ACCOUNT_LOGIN,
+  ACCOUNT_LOGOUT,
+  SET_LOGIN_TOKEN,
+  SET_DECODED_ACCOUNT,
+  SUBMIT_NEW_POST,
+  LOADING_TRUE,
+  LOADING_FALSE
+} from "./types"
+
+// -----------------------------------------------------------------------------
+
+export const getFeaturedPosts = payload => ({
+  type: GET_FEATURED_POSTS,
+  payload
+})
+
+export const getPosts = payload => ({
+  type: GET_POSTS,
+  payload
+})
+
+export const getTopics = payload => ({
+  type: GET_TOPICS,
+  payload
+})
+
+// -----------------------------------------------------------------------------
+
+export const selectPost = payload => ({
+  type: SELECT_POST,
+  payload
+})
+
+export const selectTopic = payload => ({
+  type: SELECT_TOPIC,
+  payload
+})
+
+export const selectProfile = payload => ({
+  type: SELECT_PROFILE,
+  payload
+})
+
+// -----------------------------------------------------------------------------
+
+export const submitRegisterData = payload => ({
+  type: ACCOUNT_REGISTER,
+  payload
+})
+
+export const submitLoginData = payload => ({
+  type: ACCOUNT_LOGIN,
+  payload
+})
+
+export const logout = payload => ({
+  type: ACCOUNT_LOGOUT,
+  payload
+})
+
+// -----------------------------------------------------------------------------
+
+export const setToken = payload => ({
+  type: SET_LOGIN_TOKEN,
+  payload
+})
+
+export const setDecodedAccount = payload => ({
+  type: SET_DECODED_ACCOUNT,
+  payload
+})
+
+// -----------------------------------------------------------------------------
+
+export const submitNewPostData = payload => ({
+  type: SUBMIT_NEW_POST,
+  payload
+})
