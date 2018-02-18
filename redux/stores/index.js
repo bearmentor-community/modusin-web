e3953259df8ea67827a6f95ea5bc515a2f1db9ba
M Haidar Hanif <git@mhaidarhanif.com>
2018-02-19 03:19:09 +0700

    Enhance initialState

--- redux/stores/index.js
+++ redux/stores/index.js
@@ -10,15 +10,33 @@ export const initialState = {
     selected: {}
   },
 
+  profile: {
+    selected: {}
+  },
+
   auth: {
     token: {},
     account: {},
     login: false
   },
 
+  register: {
+    name: "",
+    email: "",
+    password: "",
+    loading: false
+  },
+
+  login: {
+    email: "",
+    password: "",
+    loading: false
+  },
+
   new: {
     title: "",
     content: "",
+    loading: false,
     createdAt: "",
     updatedAt: ""
   }
