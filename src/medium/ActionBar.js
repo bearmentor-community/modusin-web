e40370712258a318ada1623dcaebd405ef144ca9
M Haidar Hanif <git@mhaidarhanif.com>
2018-02-18 20:26:04 +0700

    Polish the create post page with various blinks

--- src/medium/ActionBar.js
+++ src/medium/ActionBar.js
@@ -1,42 +1,14 @@
 import React from "react"
 import { Row, Col } from "reactstrap"
-import { Link } from "react-router-dom"
 
 import Logo from "../small/Logo"
+import DebugLinks from "../small/DebugLinks"
 
 const ActionBar = (props) => (
   <Row>
     <Col>
       <Logo />
-      <ul>
-        <li>
-          <Link to="/posts">All Posts</Link>
-        </li>
-        <li>
-          <Link to="/topics">All Topics</Link>
-        </li>
-        <li>
-          <Link to="/p/1">Post with id: 1</Link>
-        </li>
-        <li>
-          <Link to="/t/1">Topic with id: 1</Link>
-        </li>
-        <li>
-          <Link to="/register">Register</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/create">Create Post</Link>
-        </li>
-        <li>
-          <Link to="/profile">Profile</Link>
-        </li>
-        <li>
-          <Link to="/setting">Setting</Link>
-        </li>
-      </ul>
+      <DebugLinks />
     </Col>
   </Row>
 )
--- src/pages/create.js
+++ src/pages/create.js
@@ -1,14 +1,45 @@
 import React from "react"
+import { Row, Col } from "reactstrap"
+import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap"
+
 import { default as Layout } from "../templates/LayoutSingle"
 
 const Create = (props) => (
   <Layout>
-    <h3>Create New Post</h3>
-    <form>
-      <input name="post-content" id="post-title" type="text" />
-      <textarea name="post-content" id="post-content" cols="30" rows="100" />
-      <input type="button" value="Submit Post" />
-    </form>
+    <Row className="center">
+      <Col xs={10}>
+        <h3>Create New Post</h3>
+      </Col>
+      <Col xs={10}>
+        <Form>
+          <FormGroup row>
+            <Col>
+              <Input
+                type="text"
+                name="post-title"
+                id="post-title"
+                placeholder="Your post title here"
+              />
+            </Col>
+          </FormGroup>
+          <FormGroup row>
+            <Col>
+              <Input
+                type="textarea"
+                name="text"
+                id="exampleext"
+                placeholder="Write your post story here...."
+              />
+            </Col>
+          </FormGroup>
+          <FormGroup row>
+            <Col>
+              <Button>Save Post</Button>
+            </Col>
+          </FormGroup>
+        </Form>
+      </Col>
+    </Row>
   </Layout>
 )
 
